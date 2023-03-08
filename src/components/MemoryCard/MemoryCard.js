27aba0cb9e3793e5524cf542c623efe22198a172
emrezeytun <emrezeytun35@gmail.com>
2023-03-08 22:54:24 +0300

    logic completed

--- src/components/MemoryCard/MemoryCard.js
+++ src/components/MemoryCard/MemoryCard.js
@@ -4,33 +4,79 @@ import { MemoryCardItem } from "../index";
 
 export default function MemoryCard() {
   const [cardItems, setCardItems] = useState([]);
+  const [clickTracking, setClickTracking] = useState(0);
+  const [handleItems, setHandleItems] = useState([]);
 
   useEffect(() => {
     const items = [
-      { id: 1, number: 25, isOpened: true },
-      { id: 2, number: 25, isOpened: false },
-      { id: 3, number: 30, isOpened: true },
-      { id: 4, number: 30, isOpened: false },
-      { id: 5, number: 35, isOpened: false },
-      { id: 6, number: 35, isOpened: false },
-      { id: 7, number: 40, isOpened: false },
-      { id: 8, number: 40, isOpened: false },
-      { id: 9, number: 45, isOpened: false },
-      { id: 10, number: 45, isOpened: false },
-      { id: 11, number: 50, isOpened: false },
-      { id: 12, number: 50, isOpened: false },
-      { id: 13, number: 55, isOpened: false },
-      { id: 14, number: 55, isOpened: false },
-      { id: 15, number: 60, isOpened: false },
-      { id: 16, number: 60, isOpened: false },
+      { id: 1, number: 25, isOpened: true, isCorrect: true },
+      { id: 2, number: 25, isOpened: true, isCorrect: true },
+      { id: 3, number: 30, isOpened: false, isCorrect: false },
+      { id: 4, number: 30, isOpened: false, isCorrect: false },
+      { id: 5, number: 35, isOpened: false, isCorrect: false },
+      { id: 6, number: 35, isOpened: false, isCorrect: false },
+      { id: 7, number: 40, isOpened: false, isCorrect: false },
+      { id: 8, number: 40, isOpened: false, isCorrect: false },
+      { id: 9, number: 45, isOpened: false, isCorrect: false },
+      { id: 10, number: 45, isOpened: false, isCorrect: false },
+      { id: 11, number: 50, isOpened: false, isCorrect: false },
+      { id: 12, number: 50, isOpened: false, isCorrect: false },
+      { id: 13, number: 55, isOpened: false, isCorrect: false },
+      { id: 14, number: 55, isOpened: false, isCorrect: false },
+      { id: 15, number: 60, isOpened: false, isCorrect: false },
+      { id: 16, number: 60, isOpened: false, isCorrect: false },
     ];
     setCardItems(items);
   }, []);
 
+  useEffect(() => {
+    console.log("render");
+  }, [clickTracking]);
+
+  const checkCardItems = (filteredData) => {
+    if (filteredData.length > 1) {
+      if (filteredData.every((val, i, arr) => val.number === arr[0].number)) {
+        setCardItems(
+          cardItems.map((data) => {
+            return {
+              ...data,
+              isOpened: !data.isCorrect
+                ? data.number === filteredData[0].number
+                : true,
+              isCorrect: !data.isCorrect
+                ? data.number === filteredData[0].number
+                : true,
+            };
+          })
+        );
+      } else {
+        setCardItems(
+          cardItems.map((data) => {
+            return {
+              ...data,
+              isOpened: !data.isCorrect ? false : true,
+            };
+          })
+        );
+      }
+    } else {
+      setCardItems(cardItems);
+    }
+    setClickTracking(clickTracking + 1);
+  };
+
   const handleClick = (item) => {
     cardItems.find((data) => data.id === item.id).isOpened = true;
+    const filteredData = cardItems.filter(
+      (data) => !data.isCorrect && data.isOpened
+    );
+
     setCardItems(cardItems);
-    console.log("cardItems", cardItems);
+    setClickTracking(clickTracking + 1);
+
+    setTimeout(() => {
+      checkCardItems(filteredData);
+    }, 1000);
   };
   return (
     <div className="memory-card">
--- src/components/MemoryCardItem/MemoryCardItem.js
+++ src/components/MemoryCardItem/MemoryCardItem.js
@@ -5,11 +5,9 @@ export default function MemoryCardItem({ item, handleClick }) {
   return (
     <div
       onClick={(item) => handleClick(item)}
-      className={
-        item.isOpened
-          ? "memory-card-item memory-card-item-opened"
-          : "memory-card-item memory-card-item-closed"
-      }
+      className={`memory-card-item ${
+        item.isOpened ? "memory-card-item-opened" : "memory-card-item-closed"
+      } ${item.isCorrect ? "memory-card-item-correct" : ""}`}
     >
       <p>{item.number}</p>
     </div>
